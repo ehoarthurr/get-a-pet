430ec40b484396c56c354255613a472fbcf80dee
Arthur Danielson <arthurdanielson7@gmail.com>
2025-06-11 11:19:57 -0400

    Added login function

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,44 +2,29 @@ const User = require("../models/User");
 
 const bcrypt = require("bcrypt");
 
+const createUserToken = require("../helpers/create-user-token");
+
 module.exports = class UserController {
   static async register(req, res) {
     const { name, email, phone, password, confirmPassword } = req.body;
 
     // validations
-    if (!name) {
-      res.status(422).json({
-        message: "O nome é obrigatório",
-      });
-      return;
-    }
-
-    if (!email) {
-      res.status(422).json({
-        message: "O e-mail é obrigatório",
-      });
-      return;
-    }
-
-    if (!phone) {
-      res.status(422).json({
-        message: "O telefone é obrigatório",
-      });
-      return;
-    }
-
-    if (!password) {
-      res.status(422).json({
-        message: "A senha é obrigatória",
-      });
-      return;
-    }
-
-    if (!confirmPassword) {
-      res.status(422).json({
+    const requiredFields = [
+      { field: name, message: "O nome é obrigatório" },
+      { field: email, message: "O e-mail é obrigatório" },
+      { field: phone, message: "O telefone é obrigatório" },
+      { field: password, message: "A senha é obrigatória" },
+      {
+        field: confirmPassword,
         message: "A confirmação de senha é obrigatória",
-      });
-      return;
+      },
+    ];
+
+    for (const { field, message } of requiredFields) {
+      if (!field) {
+        res.status(422).json({ message });
+        return;
+      }
     }
 
     if (password !== confirmPassword) {
@@ -73,14 +58,49 @@ module.exports = class UserController {
     try {
       const newUser = await user.save();
 
-      res.status(201).json({
-        message: "Usuário criado!",
-        newUser,
-      });
+      await createUserToken(newUser, req, res);
     } catch (error) {
       res.status(500).json({
         message: error,
       });
     }
+  }
+
+  static async login(req, res) {
+    const { email, password } = req.body;
+
+    const requiredFields = [
+      { field: email, message: "O e-mail é obrigatório" },
+      { field: password, message: "A senha é obrigatória" },
+    ];
+
+    for (const { field, message } of requiredFields) {
+      if (!field) {
+        res.status(422).json({ message });
+        return;
+      }
+    }
+
+    // check if user exists
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      res.status(422).json({
+        message: "Não há usuário cadastrado com esse e-mail!",
+      });
+      return;
+    }
+
+    // check if password matches with db password
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if (!checkPassword) {
+      res.status(422).json({
+        message: "Senha inválida!",
+      });
+      return;
+    }
+
+    await createUserToken(user, req, res);
   }
 };
